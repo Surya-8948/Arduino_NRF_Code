e0de4be30fefad3300f61e5268805b929533c8fe
Surya Bajpai <suryamanibajpai@gmail.com>
2026-09-24 12:32:33 +0530

    Add NRF24L01 receiver example code

---
+++ Arduino_Nrf_Rx_GettingStart.ino
@@ -0,0 +1,20 @@
+//Rx
+#include <SPI.h>
+#include <RF24.h>
+RF24 radio(9, 10);      // CE, CSN
+const byte address[6] = "00001";
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.print("Received : ");
+    Serial.println(text);
+  }
+}
